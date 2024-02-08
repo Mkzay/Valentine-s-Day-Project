3a84a72a5ffe35a772279c530cb1eeb92a9b8569
Mkzay <80535194+Mkzay@users.noreply.github.com>
2024-02-08 14:24:42 +0100

    ...

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 const Content = () => {
-    const onYes = () => {}
+  const [imageSetIndex, setImageSetIndex] = useState(0);
+  const [textSetIndex, setTextSetIndex] = useState(0);
+  const [buttonStyles, setButtonStyles] = useState({
+    yes: { fontSize: "1rem" },
+    no: { opacity: 1, fontSize: "4xl" },
+  });
+
+  const imageSets = ["./Pookie-P.png", "./Happy.mp4", "./cat.jpeg"];
+  const textSets = ["No", "Nooooooo", "Stopppppppppp"];
+
+  const onYes = () => {
+    setImageSetIndex(1);
+  };
+
+  const onNo = () => {
+    setImageSetIndex(2);
+    setTextSetIndex((prevIndex) => (prevIndex + 1) % textSets.length);
+    setButtonStyles((prevStyles) => ({
+      ...prevStyles,
+      no: {
+        ...prevStyles.no,
+        opacity: prevStyles.no.opacity - 0.1,
+      },
+    }));
+  };
+
   return (
     <div className="flex items-center justify-center flex-col font-Cute text-white gap-8 md:gap-10 lg:gap-8">
       <div className="flex flex-col items-center justify-center">
@@ -8,15 +35,26 @@ const Content = () => {
           Will you be my Valentine?
         </h2>
       </div>
-      <div className="flex items-center justify-center bg-white w-6/12">
-        <img className="" src="./Pookie-P.png" />
+      <div className="flex items-center justify-center w-6/12">
+        {imageSetIndex === 1 ? (
+          <video autoPlay loop className="" src={imageSets[imageSetIndex]} alt="Valentine" />
+        ) : (
+          <img className="" src={imageSets[imageSetIndex]} alt="Valentine" />
+        )}
       </div>
-      <div className="flex items-center gap-16 text-4xl font-normal">
-        <button onClick={onYes} className="border-2 bg-sky-400 border-white px-3 rounded-xl text-green-500">
+      <div className="flex items-center gap-12 text-4xl font-normal">
+        <button
+          onClick={onYes}
+          className="border-2 bg-sky-400 border-white px-3 rounded-xl text-green-500"
+        >
           Yes!
         </button>
-        <button className="border-2 bg-sky-400 border-white px-3 rounded-xl text-red-500">
-          No..
+        <button
+          onClick={onNo}
+          className="border-2 bg-sky-400 border-white px-3 rounded-xl text-red-500"
+          style={buttonStyles.no}
+        >
+          {textSets[textSetIndex]}
         </button>
       </div>
     </div>
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,7 +2,7 @@ import Content from "../Components/Content";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center py-10 bg-sky-400 bg-[url] h-screen md:h-fit md:py-8 md:px-10">
+    <div className="flex items-center justify-center py-10 bg-sky-400 bg-[url] h-screen md:py-8 md:px-10">
       <Content />
     </div>
   );
