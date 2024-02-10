de88a9ec29c7872cda848bb90128414fc3ecabdf
Mkzay <80535194+Mkzay@users.noreply.github.com>
2024-02-10 13:35:01 +0100

    ...

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div className="relative h-screen">
       <video
         autoPlay
+        loop
         muted
         className="absolute inset-0 w-full h-full object-cover"
       >
